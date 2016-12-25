293637e87409e303735dda8d11aadbce7af9f25d
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2016-12-25 18:18:01 -0200

    remove sleeps
    removendo sleeps não necessários, caso der erro adicione wait.until ao elemento.

--- classes/project.py
+++ classes/project.py
@@ -15,28 +15,21 @@ class project_test(common_functions.functions,object):
 
         self.driver = driver
         wait = WebDriverWait(self.driver, 90)
-
+        self.driver.implicitly_wait(DELAY_HIGH)
         try:
 
-            self.driver.implicitly_wait(DELAY_HIGH)
-
-            print "Open page: " +str(BASE_URL + MAIN_PAGE)
+            print ("Open page: " +str(BASE_URL + MAIN_PAGE))
             self.driver.get(BASE_URL + MAIN_PAGE)
-            self.driver.implicitly_wait(DELAY_FAST)
             self.FindFrame(self.driver)
 
-            print "Find menu_name and click and button: " +str(menu_name)
+            print ("Find menu_name and click and button: " +str(menu_name))
             menu_name_test = wait.until(lambda driver: self.driver.find_element_by_link_text(menu_name))
             menu_name_test.click()
-
-            self.driver.implicitly_wait(DELAY_FAST)
             button_create = wait.until(lambda driver: driver.find_element_by_id("create"))
             button_create.click()
             self.driver.implicitly_wait(DELAY_HIGH)
             self.driver.find_element_by_name("tprojectName").clear()
-            self.driver.implicitly_wait(DELAY_HIGH)
             self.driver.find_element_by_name("tprojectName").send_keys(project_name)
-            self.driver.implicitly_wait(DELAY_HIGH)
             self.driver.find_element_by_name("tcasePrefix").clear()
             self.driver.find_element_by_name("tcasePrefix").send_keys(prefix)
             confirm_button = wait.until(lambda driver: driver.find_element_by_xpath("/html/body/div/div/form/table/tbody/tr[15]/td/div/input[3]"))
@@ -45,14 +38,12 @@ class project_test(common_functions.functions,object):
             # compare second word with user_type
             if self.FindElementXpath("/html/body/div[1]/p[1]",driver):
                     result = self.driver.find_element_by_xpath("/html/body/div[1]/p[1]").text
-                    print "The project already exists: " +str(result.encode('UTF-8'))
+                    print ("The project already exists: " +str(result.encode('UTF-8')))
                     return False
             else:
                 return True
 
         except:
             self.PrintException()
-            print "Failed class project_test, Test: ",test_number, menu_name, project_name, prefix
-            time.sleep(DELAY_HIGH)
+            print ("Failed class project_test, Test: ",test_number, menu_name, project_name, prefix)
             return False
-            pass
